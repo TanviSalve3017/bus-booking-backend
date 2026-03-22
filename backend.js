ee730e996ea37250daa219e711ae3f739a35fdd3
TanviSalve3017 <tanveesalve@gmail.com>
2026-03-22 13:56:36 +0530

    Fixed CORS issue and backend update

--- backend.js
+++ backend.js
@@ -10,23 +10,14 @@ const app = express();
 
 // ✅ १. सर्वात आधी CORS सेटअप
 app.use(cors({
-    origin: "*", 
+     origin: [
+        "http://localhost:3000",
+        "https://bus-booking-system-6omn8z1ud-tanvisalve3017s-projects.vercel.app"
+    ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With", "Accept"],
     credentials: true,
-    optionsSuccessStatus: 200 
 }));
-
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    
-    if (req.method === 'OPTIONS') {
-        return res.sendStatus(200);
-    }
-    next();
-});
 
 app.use(express.json());
 
